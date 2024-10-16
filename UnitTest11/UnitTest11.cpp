75331f45690fe72578c210b83a349c74b69a200b
kondratvlad <vladkondrat87@gmail.com>
2024-10-16 10:10:57 +0300

    first master commit

---
+++ UnitTest11/UnitTest11.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest10
+{
+    TEST_CLASS(UnitTest10)
+    {
+    public:
+
+        double g(const double x) {
+            if (abs(x) >= 1) {
+                return sin(x) / (cos(x) + 2);
+            }
+            else {
+                double S = 1; // Initial term for n=0
+                double term = 1; // a0 = 1
+                for (int n = 1; n <= 6; n++) {
+                    term *= (x * x) / (2 * n * (2 * n - 1));
+                    S += term;
+                }
+                return S;
+            }
+        }
+
+        TEST_METHOD(TestGFunctionGreaterThanOne)
+        {
+            double x = 2.0;
+            double expected = sin(x) / (cos(x) + 2);
+            double result = g(x);
+            Assert::AreEqual(expected, result, 0.0001);
+        }
+
+        TEST_METHOD(TestGFunctionLessThanOne)
+        {
+            double x = 0.5;
+            double S = 1; // Initial term for n=0
+            double term = 1; // a0 = 1
+            for (int n = 1; n <= 6; n++) {
+                term *= (x * x) / (2 * n * (2 * n - 1));
+                S += term;
+            }
+            double expected = S;
+            double result = g(x);
+            Assert::AreEqual(expected, result, 0.0001);
+        }
+    };
+}
---
+++ lab 5.03.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double g(const double x);
+
+int main()
+{
+    double zp, zk, h;
+    int n;
+
+    cout << "zp = "; cin >> zp;
+    cout << "zk = "; cin >> zk;
+    cout << "n  = "; cin >> n;
+
+    cout << fixed;
+    cout << "-----------------------------------------------" << endl;
+    cout << "|" << setw(5) << "z" << "     |"
+        << setw(20) << "result" << "              |"
+        << endl;
+    cout << "-----------------------------------------------" << endl;
+
+    double dz = (zk - zp) / n;
+    double z = zp;
+
+    while (z <= zk)
+    {
+        h = g(2 * z) + pow(g(2 * z + z * z), 2) + g(1.5);
+        cout << "|" << setw(7) << setprecision(2) << z << "   |"
+            << setw(20) << setprecision(5) << h << "              |"
+            << endl;
+        z += dz;
+    }
+    cout << "-----------------------------------------------" << endl;
+    return 0;
+}
+
+double g(const double x)
+{
+    if (abs(x) >= 1)
+        return sin(x) / (cos(x) + 2);
+    else
+    {
+        double S = 1; // Initial term for n=0
+        double term = 1; // a0 = 1
+        for (int n = 1; n <= 6; n++)
+        {
+            term *= (x * x) / (2 * n * (2 * n - 1)); 
+            S += term;
+        }
+        return S;
+    }
+}
